82b14892718b39318ac81ad121e57e63623ee35c
Robb <support@131studios.com>
2017-08-09 19:17:38 -0400

    portal

--- resources/views/sphene/portal.blade.php
+++ resources/views/sphene/portal.blade.php
@@ -5,9 +5,13 @@
 @stop
 
 @section('content')
-    <div class="text-center">
-        <iframe id="invoiceIFrame" width="100%" height="1200" style="max-width:1000px"></iframe>
-    </div>>
+    <div class="container" id="app">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <iframe id="invoiceIFrame" width="100%" height="1200" style="max-width:1000px"></iframe>
+            </div>
+        </div>
+    </div>
     <script language="javascript">
         var iframe = document.getElementById('invoiceIFrame');
         iframe.src = 'https://portal.131studios.com/view/'
