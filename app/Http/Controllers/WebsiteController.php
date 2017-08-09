0bbec901b9516a515954e5ecb90b72a4aedafaff
Robb <support@131studios.com>
2017-08-09 19:32:05 -0400

    Headers middleware

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -3,49 +3,50 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-class WebsiteController extends Controller
-{
-	/**
-	 * Constructor
-	 */
-    public function __construct() 
+class WebsiteController extends Controller {
+
+    /**
+     * Constructor
+     */
+    public function __construct()
     {
-    	//
+        //
     } // __construct
 
-    public function index() 
+    public function index()
     {
-        $projects = Project::where('hidden',0)->orderBy('lft', 'ASC')->get();
-       // $pages = Page::find(3)->withFakes();
-    	return view(env('THEME').'.index', compact('projects', 'pages'));
+        $projects = Project::where('hidden', 0)->orderBy('lft', 'ASC')->get();
+
+        // $pages = Page::find(3)->withFakes();
+        return view(env('THEME') . '.index', compact('projects', 'pages'));
     } // index
 
-    public function contact() 
+    public function contact()
     {
-     //   dd(Req::route()->uri());
-        return view(env('THEME').'.contact')->with(['heading' => 'Contact Us']);
+        //   dd(Req::route()->uri());
+        return view(env('THEME') . '.contact')->with(['heading' => 'Contact Us']);
     } // contact
 
-    public function longForm() 
+    public function longForm()
     {
-        return view(env('THEME').'.longform')->with(['heading' => 'Contact Us']);
+        return view(env('THEME') . '.longform')->with(['heading' => 'Contact Us']);
     } // longForm
 
-    public function about() 
+    public function about()
     {
-        return view(env('THEME').'.about')->with(['heading' => 'About Us']);
+        return view(env('THEME') . '.about')->with(['heading' => 'About Us']);
     } // about
 
-    public function services() 
+    public function services()
     {
-        return view(env('THEME').'.services-alt')->with(['heading' => 'Services']);
+        return view(env('THEME') . '.services-alt')->with(['heading' => 'Services']);
     } // services
 
     public function portal()
     {
-        return view(env('THEME').'.portal')->with(['heading' => 'Client Portal']);
+        return response()
+            ->view(env('THEME') . '.portal',['heading' => 'Client Portal'])
+            ->header('X-Frame-Options', ' ALLOW FROM https://131studios.com');
     }
 }
