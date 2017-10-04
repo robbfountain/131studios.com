7872dd13f3371ef3433ccf8bf77a68604623b5f3
Robb <support@131studios.com>
2017-10-03 22:03:38 -0400

    update PortalRegister request for data array

--- app/Http/Requests/PortalRegister.php
+++ app/Http/Requests/PortalRegister.php
@@ -24,9 +24,9 @@ class PortalRegister extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string',
-            'email' => 'required|email',
-            'referrer' => 'required|url|unique:portals,url'
+            'data.name' => 'required|string',
+            'data.email' => 'required|email',
+            'data.referrer' => 'required|url|unique:portals,url'
         ];
     }
     public function messages()
