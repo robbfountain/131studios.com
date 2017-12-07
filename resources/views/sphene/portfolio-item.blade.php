e021ff72c9f0a9d063253635087d7086eab6bac1
Robb Fountain <robb@131studios.com>
2017-12-07 13:58:42 -0500

    portfolio

--- resources/views/sphene/portfolio-item.blade.php
+++ resources/views/sphene/portfolio-item.blade.php
@@ -1,37 +1,34 @@
 @extends('sphene.partials.layout')
 
 @section('header')
-	@include('sphene.partials.header-sub')
+    @include('sphene.partials.header-sub')
 @stop
 
 @section('content')
-<div class="container" id="app">
-	<div class="row sticky-wrapper margin-top-50">
+    <div class="container" id="app">
+        <div class="row sticky-wrapper margin-top-50">
 
-			<!-- Photos -->
-			<div class="col-md-8">
-				<div class="project-photos">
-					<a href="#"  title="{{$project->primary_image}}" class="mfp-gallery img-hover"><img src="/{{$project->primary_image}}" alt=""></a>
-				</div>
-			</div>
+            <!-- Photos -->
+            <div class="col-md-8">
+                <div class="project-photos">
+                    <a href="#" title="{{$project->primary_image}}" class="mfp-gallery img-hover"><img
+                                src="/{{$project->primary_image}}" alt=""></a>
+                </div>
+            </div>
 
-			<!-- Content -->
-			<div class="col-md-4">
-				<div class="project-details right-side sticky">
-					<h4 class="headline">Project Description</h4>
-					<p>{!!$project->description!!}</p>
+            <!-- Content -->
+            <div class="col-md-4">
+                <div class="project-details right-side">
+                    <h4 class="headline">Project Description</h4>
+                    <p>{!!$project->description!!}</p>
 
-					<ul class="details">
-						<li><span>Date:</span> 11 January 2016</li>
-						<li><span>Location:</span> London</li>
-						<li><span>Category:</span> {{$project->sub_title}}</li>
-					</ul>
+                    <ul class="details">
+                        <li><span>Website:</span></li>
+                        <li><span>Category:</span> {{$project->sub_title}}</li>
+                    </ul>
+                </div>
+            </div>
 
-					{{-- <a href="#" class="button fw medium border margin-top-15">Launch Project</a> --}}
-
-				</div>
-			</div>
-
-	</div>
-</div>
+        </div>
+    </div>
 @stop
