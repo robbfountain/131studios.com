0a0e805804af9523f21227920160a5f46b713c34
Robb <Robb@131studios.com>
2018-02-13 20:16:24 -0500

    hosting blade

--- resources/views/sphene/services/hosting.blade.php
+++ resources/views/sphene/services/hosting.blade.php
@@ -96,7 +96,7 @@
                             <li>DNS Hosting</li>
                             <li>Advanced Logging</li>
                         </ul>
-                       
+
                     </div>
                 </div>
             </div>
