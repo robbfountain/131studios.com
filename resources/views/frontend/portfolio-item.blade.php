94b03409c17d173130646ae452670e3eb24e0aa3
Robb Fountain <robb@131studios.com>
2018-03-29 15:17:47 -0400

    added link back to projects

--- resources/views/frontend/portfolio-item.blade.php
+++ resources/views/frontend/portfolio-item.blade.php
@@ -12,7 +12,7 @@
             <div class="col-md-8">
                 <div class="project-photos">
                     <a href="#" title="{{$project->title}}" class="mfp-gallery img-hover">
-                        <img src="{{$project->imagePath()}}" alt="" >
+                        <img src="{{$project->imagePath()}}" alt="">
                     </a>
                 </div>
             </div>
@@ -27,7 +27,15 @@
                         <li><span>Website:</span> <a href="{{$project->url}}" target="_blank">{{$project->url}}</a></li>
                         <li><span>Category:</span> {{$project->category->name}}</li>
                     </ul>
+                    <div>
+                        <a href="{{route('projects')}}">
+                            &laquo; Back to Projects
+                        </a>
+                    </div>
+
                 </div>
+
+
             </div>
 
         </div>
