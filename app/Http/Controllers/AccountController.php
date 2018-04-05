9fe505f8ff784dd44b7b5e1138d686a7d2ad5219
Robb <robb@131studios.com>
2018-04-04 20:18:03 -0400

    account

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use InvoiceNinja\Config as NinjaConfig;
+use InvoiceNinja\Models\Client;
 
 class AccountController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
+        NinjaConfig::setURL('https://client.131studios.com/api/v1');
+        NinjaConfig::setToken(env('NINJA_TOKEN'));
     }
     
     public function index()
     {
-        return view('account.index');
+        return view('account.index')->with(['client' => Client::find(auth()->user()->account_id)]);
     }
 }
--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -1,13 +1,13 @@
 @extends('account.partials.layout')
 
 @section('content')
-    <nav class="flex items-center justify-between flex-wrap bg-teal p-6">
+    <nav class="flex items-center justify-between flex-wrap bg-blue-darker p-6">
         <div class="flex items-center flex-no-shrink text-white mr-6">
             <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
                  xmlns="http://www.w3.org/2000/svg">
                 <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
             </svg>
-            <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+            <span class="font-semibold text-xl tracking-normal">131 Studios</span>
         </div>
         <div class="block lg:hidden">
             <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
@@ -20,16 +20,12 @@
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
                 <a href="#responsive-header"
-                   class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-                    Docs
+                   class="block mt-4 lg:inline-block lg:mt-0 text-blue-lighter hover:text-white mr-4">
+                    Account
                 </a>
                 <a href="#responsive-header"
-                   class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
-                    Examples
-                </a>
-                <a href="#responsive-header"
-                   class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white">
-                    Blog
+                   class="block mt-4 lg:inline-block lg:mt-0 text-blue-lighter hover:text-white mr-4">
+                    Invoices
                 </a>
             </div>
             <div>
