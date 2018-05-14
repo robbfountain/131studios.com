2026626effbe28f559a2674776660c9a49f0eccc
Robb Fountain <robb@131studios.com>
2018-05-14 15:20:23 -0400

    support widget

--- resources/views/frontend/partials/layout.blade.php
+++ resources/views/frontend/partials/layout.blade.php
@@ -159,6 +159,19 @@
 {{--<script type="text/javascript" src="/js/sweetalert-dev.js"></script>--}}
 <script type="text/javascript" src="/js/custom.js"></script>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/5af9dcf0227d3d7edc254e50/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 
 </body>
 </html>
