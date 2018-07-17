1cd297027a7c5173d402443b94bcad0c29efd09d
Robb Fountain <robb@131studios.com>
2018-07-17 16:07:04 -0400

    wip

--- resources/assets/js/components/Contact.vue
+++ resources/assets/js/components/Contact.vue
@@ -10,52 +10,76 @@
                     <div class="p-2 bg-indigo-darker items-center text-indigo-lightest leading-none lg:rounded-full flex lg:inline-flex"
                          role="alert">
                         <span class="flex rounded-full bg-indigo uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
-                        <span class="font-semibold mr-2 text-left flex-auto">If you would like a quote for specific design/development work, fill out our <router-link to="/quote" class="text-white">quote form</router-link></span>
-                        <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <span class="font-semibold mr-2 text-left flex-auto">If you would like a quote for specific design/development work, fill out our <router-link
+                                to="/quote" class="text-white">quote form</router-link></span>
+                        <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 20 20">
                             <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/>
                         </svg>
                     </div>
                 </div>
+            </div>
+        </section>
+        <section class="py-8 px-2">
+            <div class="container mx-auto">
+                <div class="flex justify-between">
+                    <form @submit.prevent>
+                        <label for="name" class="block text-grey-darker text-sm font-bold mb-1">Your Name</label>
+                        <input name="name" type="text" id="name"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight"
+                               :class="form.errors.has('name') ? 'border-red' : ''" placeholder="Your Name"
+                               v-model="form.name"/>
+                        <div class="font-xs italic text-red mt-2" v-show="form.errors.has('name')">
+                            {{form.errors.get('name')}}
+                        </div>
+                        <label for="email" class="block text-grey-darker text-sm font-bold mb-1 mt-4">Your Email</label>
+                        <input name="email" type="text" id="email"
+                               class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight"
+                               :class="form.errors.has('email') ? 'border-red' : ''" placeholder="Your Email"
+                               v-model="form.email"/>
+                        <div class="font-xs italic text-red mt-2" v-show="form.errors.has('email')">
+                            {{form.errors.get('email')}}
+                        </div>
 
+                        <label for="message" class="block text-grey-darker text-sm font-bold mb-1 mt-4">Message</label>
+                        <textarea name="message" id="message" rows="5"
+                                  class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight"
+                                  :class="form.errors.has('message') ? 'border-red' : ''" placeholder=""
+                                  v-model="form.message"/>
+                        <div class="font-xs italic text-red mt-2" v-show="form.errors.has('message')">
+                            {{form.errors.get('message')}}
+                        </div>
 
-
-                <form @submit.prevent>
-                    <label for="name" class="block text-grey-darker text-sm font-bold mb-1">Your Name</label>
-                    <input name="name" type="text" id="name"
-                           class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight"
-                           :class="form.errors.has('name') ? 'border-red' : ''" placeholder="Your Name"
-                           v-model="form.name"/>
-                    <div class="font-xs italic text-red mt-2" v-show="form.errors.has('name')">
-                        {{form.errors.get('name')}}
+                        <button @click="submitMessage"
+                                class="mt-8 rounded-full border border-blue-darker text-blue-darker px-4 py-3 hover:bg-blue-darker hover:text-white">
+                            Send Message
+                        </button>
+                    </form>
+                    <div>
+                        <div>
+                            <div>
+                                <fa icon="phone"></fa>
+                                (717) 977-3306
+                            </div>
+                            <div>
+                                <fa icon="map-pin"></fa>
+                                P.O. Box 704 | Greencastle | PA | 17225
+                            </div>
+                            <div>
+                                <fa icon="envelope"></fa>
+                                <a href="mailto:contact@131studios.com">contact@131studios.com</a>
+                            </div>
+                        </div>
+                        <div>
+                            <iframe frameborder="0" class="w-full border-t"
+                                    src="https://www.google.com/maps/embed/v1/place?q=Greencastle%20PA&key=AIzaSyDMdkeuPSK3DbuZvCTK4W6WBnPnho_K54k"
+                                    allowfullscreen></iframe>
+                        </div>
                     </div>
-                    <label for="email" class="block text-grey-darker text-sm font-bold mb-1 mt-4">Your Email</label>
-                    <input name="email" type="text" id="email"
-                           class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight"
-                           :class="form.errors.has('email') ? 'border-red' : ''" placeholder="Your Email"
-                           v-model="form.email"/>
-                    <div class="font-xs italic text-red mt-2" v-show="form.errors.has('email')">
-                        {{form.errors.get('email')}}
-                    </div>
-
-                    <label for="message" class="block text-grey-darker text-sm font-bold mb-1 mt-4">Message</label>
-                    <textarea name="message" id="message" rows="5"
-                              class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight"
-                              :class="form.errors.has('message') ? 'border-red' : ''" placeholder=""
-                              v-model="form.message"/>
-                    <div class="font-xs italic text-red mt-2" v-show="form.errors.has('message')">
-                        {{form.errors.get('message')}}
-                    </div>
-
-                    <button @click="submitMessage"
-                            class="mt-8 rounded-full border border-blue-darker text-blue-darker px-4 py-3 hover:bg-blue-darker hover:text-white">
-                        Send Message
-                    </button>
-                </form>
+                </div>
             </div>
         </section>
 
-        <section>
-            <iframe frameborder="0" class="w-full border-t" src="https://www.google.com/maps/embed/v1/place?q=Greencastle%20PA&key=AIzaSyDMdkeuPSK3DbuZvCTK4W6WBnPnho_K54k" allowfullscreen></iframe> </section>
     </div>
 </template>
 
