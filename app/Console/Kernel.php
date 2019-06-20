bac41f3496b66c778ffad6057cc3232b87e846ed
Robb Fountain <robb@131studios.com>
2019-06-20 18:50:53 -0400

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('sitemap:generate')->weekly();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
