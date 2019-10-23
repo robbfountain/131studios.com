e27595c920b3b1a6d458f51ec3fb78334eabf3a8
Robb Fountain <robb@131studios.com>
2019-10-22 20:40:56 -0400

    wip

--- resources/views/frontend/contact-form.blade.php
+++ resources/views/frontend/contact-form.blade.php
@@ -1,47 +1,40 @@
 <contact inline-template>
-    <div>
-        <div class="text-sm bg-blue-lighter rounded border-blue-light text-blue p-3 mb-6">
-            <fa :icon="['far','info-circle']" ></fa>
-            Drop us a line and we'll get back to you ASAP.
+    <form @submit.prevent>
+        <label for="name" class="block text-blue-800 mb-1">Your Name</label>
+        <input name="name" type="text" id="name"
+               class="border rounded w-full py-3 px-4 text-gray-600 leading-tight"
+               :class="{'border-red' : form.errors.has('name')}" placeholder=""
+               v-model="form.name"/>
+        <div class="text-xs italic text-red-500 mt-2" v-show="form.errors.has('name')">
+            @{{form.errors.get('name')}}
         </div>
 
-        <form @submit.prevent>
-            <label for="name" class="block text-gray-600 text-sm font-bold mb-1">Your Name</label>
-            <input name="name" type="text" id="name"
-                   class="border rounded w-full py-3 px-4 text-gray-600 leading-tight"
-                   :class="{'border-red' : form.errors.has('name')}" placeholder=""
-                   v-model="form.name" />
-            <div class="text-xs italic text-red mt-2" v-show="form.errors.has('name')">
-                @{{form.errors.get('name')}}
-            </div>
+        <label for="email" class="block text-blue-800 mt-3 mb-1 mt-4">Your Email</label>
+        <input name="email" type="text" id="email"
+               class="border rounded w-full py-3 px-4 text-gray-600 leading-tight"
+               :class="{'border-red' : form.errors.has('email')}" placeholder=""
+               v-model="form.email"/>
+        <div class="text-xs italic text-red-500 mt-2" v-show="form.errors.has('email')">
+            @{{form.errors.get('email')}}
+        </div>
 
-            <label for="email" class="block text-gray-600 text-sm font-bold mb-1 mt-4">Your Email</label>
-            <input name="email" type="text" id="email"
-                   class="border rounded w-full py-3 px-4 text-gray-600 leading-tight"
-                   :class="{'border-red' : form.errors.has('email')}" placeholder=""
-                   v-model="form.email" />
-            <div class="text-xs italic text-red mt-2" v-show="form.errors.has('email')">
-                @{{form.errors.get('email')}}
-            </div>
+        <label for="message" class="block text-blue-800 mt-3 mb-1">Message</label>
+        <textarea name="message" id="message" rows="5"
+                  class="border rounded w-full py-3 px-4 text-gray-600 leading-tight"
+                  :class="{'border-red' : form.errors.has('message')}" placeholder=""
+                  v-model="form.message"></textarea>
+        <div class="text-xs italic text-red-500 mt-2" v-show="form.errors.has('message')">
+            @{{form.errors.get('message')}}
+        </div>
 
-            <label for="message" class="block text-gray-600 text-sm font-bold mb-1 mt-4">Message</label>
-            <textarea name="message" id="message" rows="5"
-                      class="border rounded w-full py-3 px-4 text-gray-600 leading-tight"
-                      :class="{'border-red' : form.errors.has('message')}" placeholder=""
-                      v-model="form.message" ></textarea>
-            <div class="text-xs italic text-red mt-2" v-show="form.errors.has('message')">
-                @{{form.errors.get('message')}}
-            </div>
-
-            <button type="submit" @click="send"
-                    class="mt-8 rounded-full border border-blue-darker text-blue-darker px-4 py-3 hover:bg-blue-darker hover:text-white">
+        <button type="submit" @click="send"
+                class="mt-8 rounded border border-blue-800 text-blue-800 px-4 py-3 hover:bg-blue-800 hover:text-white">
                <span v-if="form.busy">
                    <fa :icon="['fal','spinner']" spin></fa>
                </span>
-                <span v-else>
+            <span v-else>
                      Send Message
                 </span>
-            </button>
-        </form>
-    </div>
+        </button>
+    </form>
 </contact>
--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -11,40 +11,15 @@
     @endbanner
     <section class="xs:py-3 lg:py-10">
         <div class="container mx-auto">
-
-
-            <div class="flex lg:flex-row xs:flex-col">
-                <div class="xs:w-full lg:w-3/4 xs:mr-0 lg:mr-8 xs:mb-6 lg:mb-0">
+            <div class="flex lg:flex-row xs:flex-col pt-4">
+                <div class="xs:w-full lg:w-1/4 mr-8">
+                    <h2 class="text-lg text-blue-800 mb-3">Contact Us</h2>
+                    <p class="text-base text-gray-600">Drop us a line and we'll get back to you as soon as possible.</p>
+                </div>
+                <div class="xs:w-full lg:w-1/2 xs:mr-0 xs:mb-6 lg:mb-0">
                     @include('frontend.contact-form')
                 </div>
-                <div class="xs:w-full lg:w-1/4">
-                    <div class="flex">
-                        <div class="mr-3">
-                            <fa :icon="['fal','phone']" size="lg"></fa>
-                        </div>
-                        <div class="flex flex-col">
-                            <h4 class="roboto font-semibold text-base mb-3">Phone</h4>
-                            <div class="text-sm">
-                                <a href="tel:1-301-992.0962" class="text-gray-600 no-underline">(301) 992-0962</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="flex mt-6">
-                        <div class="mr-3">
-                            <fa :icon="['fal','envelope']" size="lg"></fa>
-                        </div>
-                        <div class="flex flex-col">
-                            <h4 class="roboto font-semibold text-base mb-3">Email</h4>
-                            <div class="text-sm">
-                                <a href="mailto:robb@131studios.com" class="text-gray-600 no-underline">robb@131studios.com</a>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
             </div>
-
         </div>
     </section>
 
