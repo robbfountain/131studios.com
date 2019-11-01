dcf51a6faceee249c901b0558b89d76cfb9f0b27
Robb Fountain <robb@131studios.com>
2019-10-31 20:41:23 -0400

    wip

--- app/Classes/SignatureValidator.php
+++ app/Classes/SignatureValidator.php
@@ -5,20 +5,24 @@ namespace App\Classes;
 
 use Illuminate\Http\Request;
 use Spatie\WebhookClient\WebhookConfig;
-use \Spatie\WebhookClient\SignatureValidator\SignatureValidator as SpatieSignatureValidator;
+use Spatie\WebhookClient\SignatureValidator\SignatureValidator as SpatieSignatureValidator;
 
 
 class SignatureValidator implements SpatieSignatureValidator
 {
 
+    /**
+     * @param Request       $request
+     * @param WebhookConfig $config
+     *
+     * @return bool
+     */
     public function isValid(Request $request, WebhookConfig $config): bool
     {
-        if(!$request->has('secret'))
-        {
+        if (!$request->has('secret')) {
             return false;
         }
 
         return $request->get('secret') == $config->signingSecret;
-
     }
 }
