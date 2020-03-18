22233526fa17d8bac09e9a225225503d5f527943
Robb Fountain <robb@131studios.com>
2020-03-18 11:09:23 -0400

    Update blogitem.blade.php

--- resources/views/frontend/blog/blogitem.blade.php
+++ resources/views/frontend/blog/blogitem.blade.php
@@ -25,7 +25,7 @@
         @if($blog->tweet)
             <span class="ml-2 text-gray-500">
                 [<a class="text-gray-500 hover:text-gray-700 mx-1"
-                    href="{{$blog->tweet}}">Twitter</a>]
+                    href="{{$blog->getLinkToFullPost()}}">Twitter</a>]
             </span>
         @endif
     </div>
