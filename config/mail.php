e1d0898358e9690a5b1e99f3ff04aed1de1c1b98
Robb Fountain <robb@131studios.com>
2020-03-18 20:53:02 -0400

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'robb@131studios.com'),
+        'name' => env('MAIL_FROM_NAME', '131 Studios'),
     ],
 
     /*
