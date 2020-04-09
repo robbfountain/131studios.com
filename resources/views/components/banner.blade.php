35fae832cb2581d3dc135aeef4aa56401f844471
Robb Fountain <robb@131studios.com>
2020-04-08 21:29:40 -0400

    Update banner.blade.php

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,4 @@
 <div class="bg-cover lg:py-10 xs:pb-4 flex flex-col"
-     style="background-image: url('https://res.cloudinary.com/onethirtyone/image/upload/v1558561494/131%20Studios/web-design-greencastle-pa_hlntpm.png');">
+     style="background-image: url('https://res.cloudinary.com/onethirtyone/image/upload/f_auto,q_75/v1558561494/131%20Studios/web-design-greencastle-pa_hlntpm.png');">
     {{$slot}}
 </div>
