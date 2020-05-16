a04accfc24a01c2ca3299963b3c93360ea297a9f
Robb Fountain <robb@131studios.com>
2020-05-15 22:05:00 -0400

    Update Blog.php

--- app/Nova/Blog.php
+++ app/Nova/Blog.php
@@ -93,8 +93,8 @@ class Blog extends Resource
 //
             BelongsTo::make('Category'),
 
-//            Text::make('Title')
-//                ->rules(['required', 'string']),
+            Text::make('Title')
+                ->rules(['required', 'string']),
 //
 //            Text::make('Slug')
 //                ->hideWhenCreating()
