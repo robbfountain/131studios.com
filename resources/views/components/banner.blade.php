753d19efee43a4994ec4d123c7c8fb52821643ce
Robb Fountain <robb@131studios.com>
2020-05-27 07:52:10 -0400

    wip

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,29 +1,11 @@
 <div class="relative bg-gray-50 overflow-hidden">
     <div class="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full">
         <div class="relative h-full max-w-screen-xl mx-auto">
-            <img class="absolute transform translate-y-1/2 -translate-x-1/3 origin-bottom-left opacity-25"
+            <img class="h-128 absolute left-full transform translate-y-1/2 -translate-x-1/2 origin-bottom-left opacity-10"
                  src="https://res.cloudinary.com/onethirtyone/image/upload/v1558561480/131%20Studios/logo-hero-2_rb9sf2.png">
 
-            <img class="absolute left-full transform -translate-y-1/4 -translate-x-1/2 opacity-25 origin-top-right"
-                 src="https://res.cloudinary.com/onethirtyone/image/upload/v1558561480/131%20Studios/logo-hero-2_rb9sf2.png">
-
-
-            {{--            <svg class="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">--}}
-            {{--                <defs>--}}
-            {{--                    <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">--}}
-            {{--                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />--}}
-            {{--                    </pattern>--}}
-            {{--                </defs>--}}
-            {{--                <rect width="404" height="784" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />--}}
-            {{--            </svg>--}}
-            {{--            <svg class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">--}}
-            {{--                <defs>--}}
-            {{--                    <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">--}}
-            {{--                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />--}}
-            {{--                    </pattern>--}}
-            {{--                </defs>--}}
-            {{--                <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />--}}
-            {{--            </svg>--}}
+            {{--            <img class="absolute left-full transform -translate-y-1/4 -translate-x-1/2 opacity-10 origin-top-right"--}}
+            {{--                 src="https://res.cloudinary.com/onethirtyone/image/upload/v1558561480/131%20Studios/logo-hero-2_rb9sf2.png">--}}
         </div>
     </div>
 
@@ -137,7 +119,7 @@
                             </div>
                         </div>
                         <div class="px-2 pt-2 pb-3"
-                        :class="{'hidden' : !isOpen}">
+                             :class="{'hidden' : !isOpen}">
                             <a href="{{route('website-design.index')}}"
                                class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out">
                                 Website Design</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  theme: {},
+  theme: {
+    opacity: {
+      '0': '0',
+      '10': '0.10',
+      '25': '0.25',
+      '50': '0.5',
+      '75': '0.75',
+      '100': '1',
+    },
+  },
   variants: {},
   plugins: [
     require('@tailwindcss/ui'),
