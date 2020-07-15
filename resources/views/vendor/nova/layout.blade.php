47f532bbc9d6d223c2e0cf71da65e58276de3276
Robb Fountain <robb@131studios.com>
2020-07-15 14:04:50 -0400

    Update layout.blade.php

--- resources/views/vendor/nova/layout.blade.php
+++ resources/views/vendor/nova/layout.blade.php
@@ -53,13 +53,9 @@
                 <div data-testid="content" class="px-view py-view mx-auto">
                     @yield('content')
 
-                    {{--<p class="mt-8 text-center text-xs text-80">--}}
-                        {{--<a href="http://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>--}}
-                        {{--<span class="px-1">&middot;</span>--}}
-                        {{--&copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.--}}
-                        {{--<span class="px-1">&middot;</span>--}}
-                        {{--v{{ Laravel\Nova\Nova::version() }}--}}
-                    {{--</p>--}}
+                    <p class="mt-8 text-center text-xs text-80">
+                        v{{ Laravel\Nova\Nova::version() }}
+                    </p>
                 </div>
             </div>
         </div>
