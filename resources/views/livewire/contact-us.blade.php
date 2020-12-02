fefe0970b1bcbb2e6d0a03e8d1e9026fa68f37ff
Robb Fountain <robb@131studios.com>
2020-12-01 21:41:24 -0500

    fix contact css

--- resources/views/livewire/contact-us.blade.php
+++ resources/views/livewire/contact-us.blade.php
@@ -1,51 +1,46 @@
-<form action="#" method="POST" class="grid grid-cols-1 row-gap-6" wire:submit.prevent="sendEmail">
+<form action="#" method="POST" class="grid grid-cols-1 gap-y-6" wire:submit.prevent="sendEmail">
     <div>
         <label for="full_name" class="sr-only">Full name</label>
-        <div class="relative rounded-md shadow-sm">
-            <input wire:model="name"
-                   id="full_name"
-                   class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
-                   placeholder="Full name"/>
-            @error('name') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
-        </div>
+        <input wire:model="name"
+               id="full_name"
+               autocomplete="name"
+               class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md"
+               placeholder="Full name"/>
+        @error('name') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
     </div>
     <div>
         <label for="email" class="sr-only">Email</label>
-        <div class="relative rounded-md shadow-sm">
-            <input wire:model="email"
-                   id="email"
-                   type="email"
-                   class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
-                   placeholder="Email"/>
-            @error('email') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
-        </div>
+        <input wire:model="email"
+               id="email"
+               type="email"
+               autocomplete="email"
+               class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md"
+               placeholder="Email"/>
+        @error('email') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
     </div>
     <div>
         <label for="phone" class="sr-only">Phone</label>
-        <div class="relative rounded-md shadow-sm">
-            <input wire:model="phone"
-                   id="phone"
-                   class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
-                   placeholder="Phone"/>
-            @error('phone') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
-        </div>
+        <input wire:model="phone"
+               id="phone"
+               autocomplete="phone"
+               class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md"
+               placeholder="Phone"/>
+        @error('phone') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
     </div>
     <div>
         <label for="message" class="sr-only">Message</label>
-        <div class="relative rounded-md shadow-sm">
-            <textarea wire:model="message"
-                      id="message"
-                      rows="4"
-                      class="form-input block w-full py-3 px-4 placeholder-gray-500 transition ease-in-out duration-150"
-                      placeholder="Message">
-            </textarea>
-            @error('message') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
-        </div>
+        <textarea wire:model="message"
+                  id="message"
+                  rows="4"
+                  class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border border-gray-300 rounded-md"
+                  placeholder="Message">
+        </textarea>
+        @error('message') <span class="text-sm text-red-500">{{ $message }}</span> @enderror
     </div>
     <div class="flex items-center">
         <span class="inline-flex rounded-md shadow-sm">
             <button type="submit"
-                    class="inline-flex justify-center py-3 px-6 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-indigo active:bg-blue-700 transition duration-150 ease-in-out">
+                    class="inline-flex justify-center py-3 px-6 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out">
                 <span wire:loading wire:target="sendEmail">
                     <i class="fal fa-spinner fa-spin mr-1"></i>
                 </span>
