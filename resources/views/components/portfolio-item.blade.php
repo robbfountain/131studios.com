1cc1c60a646bf1fec4c88d312a6d13b1cb28cde3
Robb Fountain <robb@131studios.com>
2020-12-01 21:52:40 -0500

    Update portfolio-item.blade.php

--- resources/views/components/portfolio-item.blade.php
+++ resources/views/components/portfolio-item.blade.php
@@ -5,18 +5,18 @@
     <div class="flex-1 bg-white p-6 flex flex-col justify-between">
         <div class="flex-1">
             <p class="text-sm leading-5 font-medium text-blue-600">
-                <a href="#" class="hover:underline">
+                <span>
                     Project
-                </a>
+                </span>
             </p>
-            <a href="#" class="block">
+            <span class="block">
                 <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
                     {{$project->title}}
                 </h3>
                 <p class="mt-3 text-base leading-6 text-gray-500">
                     {!! $project->getPreview() !!}
                 </p>
-            </a>
+            </span>
             <div class="mt-6">
                 <a href="{{$project->url}}"
                    class="text-sm text-blue-500 hover:text-blue-700 transition duration-100 ease-in-out">
