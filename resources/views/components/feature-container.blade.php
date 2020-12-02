d282badd4788e39e9eaf28a51ff1342d3ff2cddf
Robb Fountain <robb@131studios.com>
2020-12-01 22:12:13 -0500

    Update feature-container.blade.php

--- resources/views/components/feature-container.blade.php
+++ resources/views/components/feature-container.blade.php
@@ -1,5 +1,5 @@
 <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-    <ul class="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+    <ul class="md:grid md:grid-cols-2 md:gap-y-8 md:gap-x-10">
         {{$slot}}
     </ul>
 </div>
