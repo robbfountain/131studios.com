0c028163c222dab5c436ab43f030d68bebcafad3
Robb Fountain <robb@131studios.com>
2021-10-11 07:40:20 -0400

    fixed 500 errors

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -4,11 +4,10 @@
     <link rel="canonical" href="{{route('contact.index')}}/" />
     <meta name="description"
           content="Web Design, Social Media, Hosting and SEO for Greencastle, Chambersburg and Hagerstown.  Get your FREE SEO Checkup Today."/>
-    <meta name="twitter:card" content="131 Studios"/>
+    <meta name="twitter:card" content="131 Studios - Contact Us"/>
     <meta name="twitter:site" content="@131Studios"/>
     <meta property="twitter:title" content="131 Studios">
     <meta property="twitter:description" content="Web Design, Social Media, Hosting and SEO for Greencastle, Chambersburg and Hagerstown.  Get your FREE SEO Checkup Today.">
-    <meta property="twitter :image" content="{{$blog->imageUrl()}}">
     <meta name="twitter:creator" content="@131Studios"/>
     <meta property="og:site_name" content="{{config('app.name')}}">
     <meta property="og:url" content="{{route('contact.index')}}">
