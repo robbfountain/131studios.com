bce1702bb0f66cd21150414027710b616f9afed2
Robb Fountain <robb@131studios.com>
2021-10-10 22:16:37 -0400

    wip

--- resources/views/frontend/social-media.blade.php
+++ resources/views/frontend/social-media.blade.php
@@ -7,7 +7,6 @@
     <meta name="twitter:site" content="@131Studios"/>
     <meta property="twitter:title" content="131 Studios">
     <meta property="twitter:description" content="Web Design, Social Media, Hosting and SEO for Greencastle, Chambersburg and Hagerstown.  Get your FREE SEO Checkup Today.">
-    <meta property="twitter :image" content="{{$blog->imageUrl()}}">
     <meta name="twitter:creator" content="@131Studios"/>
     <meta property="og:site_name" content="{{config('app.name')}}">
     <meta property="og:url" content="{{route('social-media.index')}}">
