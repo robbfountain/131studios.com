a1e951adf8d82861bc053647ec1e1727bb8e91f3
Robb Fountain <robb@131studios.com>
2021-11-04 21:37:38 -0400

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -12,6 +12,7 @@
     <meta property="og:site_name" content="{{config('app.name')}}">
     <meta property="og:url" content="{{url('/')}}">
     <meta property="og:title" content="131 Studios">
+    <meta property="og:type" content="website" />
     <meta property="og:description"
           content="Web Design, Social Media, Hosting and SEO for Greencastle, Chambersburg and Hagerstown.  Get your FREE SEO Checkup Today.">
     <link rel="canonical" href="{{url('/')}}">
