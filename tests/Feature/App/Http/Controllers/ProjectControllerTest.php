06055abc79e0f26f9f1a320270d858ccf9b40ca3
Robb Fountain <robb@131studios.com>
2022-02-15 20:57:49 -0500

    updating test

--- tests/Feature/App/Http/Controllers/ProjectControllerTest.php
+++ tests/Feature/App/Http/Controllers/ProjectControllerTest.php
@@ -2,12 +2,20 @@
 
 namespace Tests\Feature\App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ProjectControllerTest extends TestCase
 {
     use DatabaseMigrations;
+
+    public function setUp():void
+    {
+        parent::setUp();
+
+        Storage::fake();
+    }
 
     /** @test * */
     public function Index_returns_a_view()
