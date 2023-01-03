3fb53c2463fa0e6f3761a6ded6c3a9d78117b3f3
Robb Fountain <robb@131studios.com>
2023-01-03 11:28:13 -0500

    wip

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['bugsnag','daily'],
             'ignore_exceptions' => false,
         ],
 
