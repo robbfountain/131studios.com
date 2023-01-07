0ad2140c2de50c20f5f5efad980bebc1bf2989c7
Robb Fountain <robb@131studios.com>
2023-01-06 22:43:54 -0500

    wip

--- app/Console/Commands/BrowserShotCommand.php
+++ app/Console/Commands/BrowserShotCommand.php
@@ -7,6 +7,7 @@ use App\Classes\ProjectReader;
 use Illuminate\Console\Command;
 use App\Classes\Filters\Hidden;
 use Spatie\Browsershot\Browsershot;
+use Illuminate\Support\Facades\Storage;
 
 class BrowserShotCommand extends Command
 {
@@ -47,20 +48,20 @@ class BrowserShotCommand extends Command
             ])->get();
 
         foreach ($projects as $project) {
-            $path = public_path('images/screenshots/'.$project->slug.'.png');
-            if (file_exists($path)) {
+            if (Storage::disk('s3')->exists('131Studios/screenshots/'.$project->url.'.png')) {
                 $this->comment('Screenshot exists for '.$project->url.'...skipping');
             } else {
                 if (!is_null($project->url)) {
                     $this->line('Getting screenshot for '.$project->url);
                     try {
-                        Browsershot::url($project->url)
-                            ->windowSize(1920, 1080)
-                            ->setDelay(8000)
-                            ->fullPage()
-                            ->save($path);;
+                        Storage::disk('s3')
+                            ->put('131Studios/screenshots/'.$project->url.'.png', Browsershot::url($project->url)
+                                ->windowSize(1920, 1080)
+                                ->setDelay(8000)
+                                ->fullPage()
+                                ->screenshot());
                     } catch (\Exception $e) {
-                        $this->error('Could not resolve '.$project->url);
+                        $this->error($e->getMessage());
                     }
                 } else {
                     $this->error('No url defined');
